a48a77356b3659fcee09e9365884618dd3b27e89
GangstarZero <jasonbgangstar168@gmail.com>
2024-10-18 20:23:27 +0700

    Styling dashboard and navbar

--- resources/views/layout/guestNavbar.blade.php
+++ resources/views/layout/guestNavbar.blade.php
@@ -1,14 +1,12 @@
-<nav class="container-fluid navbar-light bg-light p-3">
+<nav class="container-fluid navbar-light p-3" style="background-color: #8B3333;">
     <div class="d-flex justify-content-between align-items-center w-100">
         <div>
             <p class="fs-5">Logo</p>
         </div>
 
         <div class="d-none d-md-flex justify-content-center position-absolute start-50 translate-middle-x">
-            <a class="nav-link fs-5 mx-4" href="{{ route('home') }}">Home</a>
-            <a class="nav-link fs-5 mx-4" href="">Apply</a>
-            <a class="nav-link fs-5 mx-4" href="">Jobs</a>
-            <a class="nav-link fs-5 mx-4" href="">Contact</a>
+            <a class="nav-link fs-5 mx-4 text-white" href="{{ route('home') }}">Home</a>
+            <a class="nav-link fs-5 mx-4 text-white" href="">Jobs</a>
         </div>
 
         <div>
@@ -17,9 +15,7 @@
     </div>
 
     <div class="d-flex d-md-none flex-column align-items-center">
-        <a class="nav-link fs-5" href="{{ route('home') }}">Home</a>
-        <a class="nav-link fs-5" href="">Apply</a>
-        <a class="nav-link fs-5" href="">Jobs</a>
-        <a class="nav-link fs-5" href="">Contact</a>
+        <a class="nav-link fs-5 text-white" href="{{ route('home') }}">Home</a>
+        <a class="nav-link fs-5 text-white" href="">Jobs</a>
     </div>
 </nav>
